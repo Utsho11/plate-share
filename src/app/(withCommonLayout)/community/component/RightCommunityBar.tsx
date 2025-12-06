e23bcace12a95159fe6b9532baadfb78ba6bda7b
Utsho11 <utshoroy521@gmail.com>
2025-12-07 00:54:02 +0600

    add filter community in community rightbar

--- src/app/(withCommonLayout)/community/component/RightCommunityBar.tsx
+++ src/app/(withCommonLayout)/community/component/RightCommunityBar.tsx
@@ -24,10 +24,20 @@ import { toast } from "sonner";
 
 const RightCommunityBar = () => {
   const { data, isLoading } = useGetAllCommunitiesQuery({});
-  const { data: myCommunities, isLoading: myCommunitiesLoading } = useGetAllMyCommunitiesQuery({});
+  const { data: myCommunities, isLoading: myCommunitiesLoading } =
+    useGetAllMyCommunitiesQuery({});
+  const { data: communities, isLoading: communitiesLoading } =
+    useGetAllCommunitiesQuery({});
   const [leaveCommunity] = useLeaveCommunityMutation();
 
-  // console.log(data);
+  console.log(communities);
+  console.log(myCommunities);
+
+  const myIds = myCommunities?.map((c) => c.community_id?._id) || [];
+
+  const filteredCommunity = communities?.filter((c) => !myIds.includes(c._id));
+
+  // console.log({ filteredCommunity });
 
   if (myCommunitiesLoading) return <div>Loading...</div>;
 
@@ -45,61 +55,82 @@ const RightCommunityBar = () => {
   };
 
   return (
-    <Card>
-      <CardHeader className="">
-        <div className="mb-4 space-y-2">
-          <Link href="/community" className="text-start w-full">
-            <Button
-              variant="outline"
-              className="w-full justify-start"
-              style={{ textDecoration: "none", padding: "0 4px" }}
-            >
-              <Newspaper /> My Feed
-            </Button>
-          </Link>
-          <Dialog>
-            <DialogTrigger className="flex items-center text-sm font-semibold py-2 px-1 hover:bg-accent mt-2 border rounded-lg gap-2 w-full">
-              <PlusCircle size={16} /> Create Community
-            </DialogTrigger>
-            <DialogContent>
-              <DialogTitle>Create Your Community</DialogTitle>
-              <CreateCommunity />
-            </DialogContent>
-          </Dialog>
-          <Link href="/community" className="text-start w-full">
-            <Button
-              variant="outline"
-              className="w-full justify-start"
-              style={{ textDecoration: "none", padding: "0 4px" }}
-            >
-              <Globe /> Explore Communities
-            </Button>
-          </Link>
-        </div>
-        <CardTitle>My Communities</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <div className="space-y-3">
-          {myCommunities.map((c: any) => (
-            <div
-              key={c._id}
-              className="p-3 border rounded-lg hover:bg-gray-100 cursor-pointer flex justify-between items-center"
-            >
-              <div>
-                <h2 className="font-medium">{c.community_id.name}</h2>
-                {/* <p className="text-sm text-gray-500">{c.} members</p> */}
-              </div>
+    <div>
+      <Card>
+        <CardHeader className="">
+          <div className="mb-4 space-y-2">
+            <Link href="/community" className="text-start w-full">
               <Button
-                variant={"destructive"}
-                onClick={() => handleLeaveCommunity(c._id)}
+                variant="outline"
+                className="w-full justify-start"
+                style={{ textDecoration: "none", padding: "0 4px" }}
               >
-                Leave
+                <Newspaper /> My Feed
               </Button>
-            </div>
-          ))}
-        </div>
-      </CardContent>
-    </Card>
+            </Link>
+            <Dialog>
+              <DialogTrigger className="flex items-center text-sm font-semibold py-2 px-1 hover:bg-accent mt-2 border rounded-lg gap-2 w-full">
+                <PlusCircle size={16} /> Create Community
+              </DialogTrigger>
+              <DialogContent>
+                <DialogTitle>Create Your Community</DialogTitle>
+                <CreateCommunity />
+              </DialogContent>
+            </Dialog>
+            <Link href="/community" className="text-start w-full">
+              <Button
+                variant="outline"
+                className="w-full justify-start"
+                style={{ textDecoration: "none", padding: "0 4px" }}
+              >
+                <Globe /> Explore Communities
+              </Button>
+            </Link>
+          </div>
+          <CardTitle>My Communities</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="space-y-3">
+            {myCommunities.map((c: any) => (
+              <div
+                key={c._id}
+                className="p-3 border rounded-lg hover:bg-gray-100 cursor-pointer flex justify-between items-center"
+              >
+                <div>
+                  <h2 className="font-medium">{c.community_id.name}</h2>
+                  {/* <p className="text-sm text-gray-500">{c.} members</p> */}
+                </div>
+                <Button
+                  variant={"destructive"}
+                  onClick={() => handleLeaveCommunity(c._id)}
+                >
+                  Leave
+                </Button>
+              </div>
+            ))}
+          </div>
+        </CardContent>
+      </Card>
+      <br />
+      <Card>
+        <CardHeader>
+          <CardTitle>All Communities</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="space-y-3">
+            {filteredCommunity.map((c: any) => (
+              <div
+                key={c._id}
+                className="flex justify-between items-center border rounded-lg p-2"
+              >
+                <p>{c.name}</p>
+                <Button variant={"outline"}>Join</Button>
+              </div>
+            ))}
+          </div>
+        </CardContent>
+      </Card>
+    </div>
   );
 };
 
